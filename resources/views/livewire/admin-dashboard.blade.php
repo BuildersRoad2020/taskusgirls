a39944372d8b8b83d44e00e31796d905e6ddc222
Builders Road <buildersroad2020@gmail.com>
2021-03-02 19:33:48 +1100

    changes

--- resources/views/livewire/admin-dashboard.blade.php
+++ resources/views/livewire/admin-dashboard.blade.php
@@ -1088,12 +1088,13 @@
                                     </x-jet-secondary-button>
                                 </x-slot>
                             </x-jet-confirmation-modal>
-
+                            @can('viewAny', App\Models\User::class)
                             <button class="border-2 border-red-200 rounded-md p-1" wire:click="confirmDelete({{$task->id}})">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-red-500">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                 </svg>
                             </button>
+                            @endcan
                         </div>
                     </td>
                     <td class="p-4 whitespace-nowrap">
